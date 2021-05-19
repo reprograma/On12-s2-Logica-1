49def83eba74ba674f9b1e1659776c5adc6a4f4e
simaraconceicao <contato@simaraconceicao.com>
2021-05-19 18:32:28 -0300

    insere revisao

---
+++ revisao/exercicios.js
@@ -0,0 +1,33 @@
+//instalar code runner
+// O que é algoritmo? sequencia finita de passos para resolver um problema
+// Dificuldade de colocar no código o que nosso cerebro pode fazer por natureza
+
+//1 - Elabore um algoritmo que receba dois números e determine qual é o menor entre eles, se os números forem iguais, mostre uma mensagem no console "Os números são iguais".
+
+
+//2 - Elabore um algoritmo que receba uma string com dia da semana e devolva um número (1-7) correspondente ao dia da semana.
+
+
+//3 - Elabore um algoritmo que receba uma string com dia da semana (sexta, sábado ou domingo) e devolva uma string: sextou, sabadou ou domingou e se for qualquer outro dia, interpolar e  informar que nao é um dia festivo. 
+
+
+//4 - Crie um algoritmo que leia o valor de um jantar, calcule e informe o valor da taxa do garçom (30%) e o valor total a ser pago.
+
+
+//5 - Crie um algoritmo que receba o valor da conta e a quantidade de amigas, calcule e informe o valor da taxa do garçom (10%) e o valor total a ser pago e valor para cada amiga.
+
+
+//6 - Crie uma algoritmo que recebe a idade da pessoa e informa se ela pode assistir ao filme com classificação de 14 anos
+
+
+//7 - Se um processo seletivo para vaga de deva tem 10 algoritmos e serao aprovados quem acertar pelo menos 7 deles, crie um sistema que informe se a candidata foi aprovada ou nao
+
+
+//8 - Execute as seguintes tarefas: 1.Declare uma nova variável chamada 'calculadora', e adicione uma instrução somando os valores 10 e 5.
+
+// Incremente 1 à variável 'calculadora', usando o operador de soma abreviado.
+// Atribua à variável 'calculadora' seu próprio valor multiplicando por 3, usando o operador de multiplicação abreviado.
+// Mostre os valores no console.
+
+
+
